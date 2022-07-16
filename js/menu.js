d57b760db2d52ce29239596c0b6ce4df52e6d7e7
BrenBrayan <bryan.brenes.pere@gmail.com>
2022-07-16 14:55:57 -0600

    Carpetas nuevas y menú
    -Carpetas para guardar imágenes creadas.
    -Menú se mantiene del mismo color al hacer scroll.
    -Eliminación de css innecesario del menú.

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,3 @@
-//Parte del funcionamiento del menú para mantenerlo fixed (sticky) al hacer scroll
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 0);
-});
-
 //Función para agregar clase ".active" al menú superior para mostrar/ocultar opciones.
 function toggleMenu(){
     const menuOption = document.querySelector('.display_menu');
