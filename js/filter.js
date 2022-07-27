03e6ce571ffc5a911b8f27352d2f322e16b0949a
BrenBrayan <bryan.brenes.pere@gmail.com>
2022-07-27 15:00:51 -0600

    Cambios en nombres de variables
    -Se cambian nombre en filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,15 @@
-filterTravel("all")
+//Manejo de todos los botones del filtro. Contiene una función para agregar y eliminar la clase "active_state" para el manejo de filtro.
+var btnContainer = document.getElementById("filter_options");
+var btns = btnContainer.getElementsByClassName("btn_option");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active_state");
+        current[0].className = current[0].className.replace(" active_state", "");
+        this.className += " active_state";
+    });
+}
+
+filterTravel("all");
 //Manejo en agregr/eliminar la clase "show_travel" de los divs con el contenido de los paquetes
 function filterTravel(type_travel) {
     var divTravel;
@@ -12,37 +23,26 @@ function filterTravel(type_travel) {
 }
 
 //Función para agregar la clase "show_travel" a los divs con la opción seleccionada en los botones.
-function addClass_Show(element, name) {
+function addClass_Show(arrayTravel, classDiv) {
     var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
+    arr1 = arrayTravel.className.split(" ");
+    arr2 = classDiv.split(" ");
     for (i = 0; i < arr2.length; i++) {
         if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
+            arrayTravel.className += " " + arr2[i];
         }
     }
 }
 
 //Función para eliminar la clase "show_travel" a los divs que no corresponden a la opción seleccionada.
-function removeClass_Show(element, name) {
+function removeClass_Show(arrayTravel, classDiv) {
     var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
+    arr1 = arrayTravel.className.split(" ");
+    arr2 = classDiv.split(" ");
     for (i = 0; i < arr2.length; i++) {
         while (arr1.indexOf(arr2[i]) > -1) {
             arr1.splice(arr1.indexOf(arr2[i]), 1);
         }
     }
-    element.className = arr1.join(" ");
-}
-
-//Manejo de todos los botones del filtro. Contiene una función para agregar y eliminar la clase "active_state" para el manejo de filtro.
-var btnContainer = document.getElementById("filter_options");
-var btns = btnContainer.getElementsByClassName("btn_option");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active_state");
-        current[0].className = current[0].className.replace(" active_state", "");
-        this.className += " active_state";
-    });
+    arrayTravel.className = arr1.join(" ");
 }
